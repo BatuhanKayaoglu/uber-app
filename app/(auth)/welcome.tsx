4a323c68d5b1d4209a5219f1f96b39b9ed66a494
Batuhan Kayaoglu <batuhan.kayaoglu@apptotech.com>
2025-03-12 16:41:52 +0300

    onboarding page started.

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,19 +1,41 @@
-import { Text, View, StyleSheet } from "react-native";
+import {SafeAreaView, Text, TouchableOpacity, View} from "react-native";
+import {router} from "expo-router";
+import Swiper from "react-native-swiper";
+import {useRef, useState} from "react";
+import {onboarding} from "@/constants";
 
 const Onboarding = () => {
+
+    const swiperRef = useRef<Swiper>(null);
+    const [activeIndex, setActiveIndex] = useState(0);
+
     return (
-        <View style={styles.container}>
-            <Text className="text-red-900 font-bold">Onboarding</Text>
-        </View>
+        <SafeAreaView className="flex h-full items-center justify-between bg-white mt-10">
+            <TouchableOpacity onPress={()=>{ router.replace("/(auth)/sign-up");
+            }}
+            className="w-full flex justify-end items-end p-5"
+            >
+                <Text className="text-black text-md font-JakartaBold">Skip</Text>
+            </TouchableOpacity>
+
+            <Swiper
+                ref={swiperRef}
+                loop={false}
+                dot={<View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full"/>}
+                activeDot={<View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full"/>}
+                onIndexChanged = {(index) => setActiveIndex(index)}
+            >
+            {onboarding.map((item, index) => (
+                <View key={index}>
+                   <Text>{item.title}</Text>
+                </View>
+            ))}
+            </Swiper>
+        </SafeAreaView>
+
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
-});
+
 
 export default Onboarding;
---
+++ constants/index.ts
@@ -0,0 +1,8 @@
+export const onboarding = [
+    {
+        id:1,
+        title: "The perfect ride is just tap a way!",
+        description: "The journey begins with Ryde",
+        image: null
+    }
+]
