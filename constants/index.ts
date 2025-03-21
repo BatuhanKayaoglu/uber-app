27bd21bd99e3e2c07eb751b2793b169aa0d52d35
Batuhan Kayaoglu <batuhan.kayaoglu@apptotech.com>
2025-03-21 17:49:03 +0300

    add customButton.tsx with mainPage

--- constants/index.ts
+++ constants/index.ts
@@ -1,8 +1,24 @@
+import onboarding1 from "../assets/images/onboarding1.png";
+import onboarding2 from "../assets/images/onboarding2.png";
+import onboarding3 from "../assets/images/onboarding3.png";
+
 export const onboarding = [
-    {
-        id:1,
-        title: "The perfect ride is just tap a way!",
-        description: "The journey begins with Ryde",
-        image: null
-    }
-]
+  {
+    id: 1,
+    title: "The perfect ride is just tap a way!",
+    description: "The journey begins with Ryde",
+    image: onboarding1,
+  },
+  {
+    id: 2,
+    title: "The perfect ride is just tap a way!",
+    description: "The journey begins with Ryde.",
+    image: onboarding2,
+  },
+  {
+    id: 3,
+    title: "The perfect ride is just tap a way!",
+    description: "The journey begins with Ryde.",
+    image: onboarding3,
+  },
+];
